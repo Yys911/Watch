56c80b820bfa22440a8126cf2f54b7c9c5c684df
Yys911 <61673811+Yys911@users.noreply.github.com>
2021-03-01 02:45:10 +0000

    Update watch.h

--- Dependence/watch.h
+++ Dependence/watch.h
@@ -4,34 +4,57 @@
  */
 
 
-#include "sensors.h"				//the library declears sensors functions
+#ifndef _WATCH_h
+#define _WATCH_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
+
+//#include "sensors.h"				//the library declears sensors functions
 #include <WiFi.h>
 #include <Wire.h>
 #include <ACROBOTIC_SSD1306.h>		//library for displaying data
 #include <ArduinoJson.h>			//library for serialization and deserialization
+#include <DFRobot_Heartrate.h>		//library for heart rate sensor
+
+//#define heartrate_pin A0
 
 
-void Debounce_function();
+void con_to_wifi(char* ssid, char* password);
+void mean(double* nums);
 void SEND(void);					//Function to send data to the server
 void RECEIVE(void);					//Function to receive data from the server
 
 
-class Data_monitoring_class {
+DFRobot_Heartrate heartrate(DIGITAL_MODE);
+
+
+class Data_monitoring {
 public:
 	void step_tracker();
 	void heartrate_tracker();
 	void calories_buring_tracker();
+	void whether_sleeping();
 
 private:
+	uint8_t heart_rate;
 	int daily_steps;
 	int calories_burning;
-	bool whether_sleeping;
+	bool sleeping;
 
 };
 
 
-class Display_class {
+class Display {
 public:
+	void ini();
 	void display_function();  //display function is used to display massage on screen
 
 private:
@@ -39,11 +62,12 @@ private:
 };
 
 
-class Response_from_sever_class {
+class Response_from_sever {
 public:
 	void vibrate_function();
 	void beep_function();
 
 private:
 	int lasting_time;
-}; #pragma once
+}; 
+#pragma once
