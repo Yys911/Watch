d5095285e95fcd1e5b93e466ea32ee151d5449ec
Yys911 <61673811+Yys911@users.noreply.github.com>
2021-03-03 17:00:58 +0000

    Update watch.h

--- Dependence/watch.h
+++ Dependence/watch.h
@@ -3,78 +3,74 @@
   by Yishun Zhao
  */
 
-#if defined(ARDUINO_ARCH_SAMD)
-    #define SerialMonitorInterface SerialUSB
-#else
-	#define SerialMonitorInterface Serial
-#endif
-
+#pragma once
 
 #ifndef _WATCH_h
 #define _WATCH_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
+#if defined(ARDUINO_ARCH_SAMD)
+#define SerialMonitorInterface SerialUSB
 #else
-	#include "WProgram.h"
-#endif
+#define SerialMonitorInterface Serial
 #endif
 
 
 #include <WiFi101.h>
+#include <PubSubClient.h>
 #include <Wire.h>
-#include <ACROBOTIC_SSD1306.h>		//library for displaying data
-#include <ArduinoJson.h>			//library for serialization and deserialization
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>		//library for displaying data
 #include <DFRobot_Heartrate.h>		//library for heart rate sensor
 
-#define xStill 1636
+#define heartrate_pin 3
+#define vibrate_pin 4
+#define beep_pin 5
+#define xStill 1636      
 #define yStill 192
 #define zStill 17536
-#define MPU 0x68	//since AD0 = LOW
+#define MPU 0x68	         //since AD0 = LOW
+#define SCREEN_WIDTH 128     // OLED display width, in pixels
+#define SCREEN_HEIGHT 32     // OLED display height, in pixels
+#define OLED_RESET -1        // Reset pin # for screen(or -1 if sharing Arduino reset pin)
 
 
-void con_to_wifi(char* ssid, char* password);
-void mean(double* nums);
-void SEND(void);					//Function to send data to the server
-void RECEIVE(void);					//Function to receive data from the server
+enum Var_name { Stepping, Heart_rate, StepCount, Calories_burning, Sleeping };
 
 
-DFRobot_Heartrate heartrate(DIGITAL_MODE);
+uint16_t mean_val(uint16_t* nums);
 
 
 class Data_monitoring {
 public:
+	friend void send(Data_monitoring data_moni);			//Function to send data to the server
+	char get_data(Var_name name);
 	void step_tracker();
+	void init_screen();
 	void init_step_tracker();
 	void heartrate_tracker();
-	void calories_buring_tracker();
+	void calculating();
 	void whether_sleeping();
+	void display_data();  //display function is used to display massage on screen
 
 private:
 	bool stepping;
+	bool sleeping;
 	uint16_t heart_rate;
 	uint16_t stepCount;
-	int calories_burning;
-	bool sleeping;
+	uint16_t calories_burning;
+	uint16_t fat_burning;
+	uint16_t distance;
 
 };
 
 
-class Display {
+class Server_related {
 public:
-	void ini();
-	void display_function();  //display function is used to display massage on screen
-
-private:
-	char data_need_to_be_displayed[];
+	void v_b(byte* sensor);
+	friend void callback(char* topic, byte* payload, unsigned int length);    //Function to receive data from the server
+	void connectWifi(const char* ssid, const char* password);
+	void connectMQTTServer(const char* mqttServer);
+	void loop(void);
 };
 
-
-class Response_from_sever {
-public:
-	void v_b(char sensor);
-
-private:
-	int lasting_time;
-}; 
-#pragma once
+#endif
