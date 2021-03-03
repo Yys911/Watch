eaab5dfad475efb39d05d744d1744d09bfdc4f07
Yys911 <61673811+Yys911@users.noreply.github.com>
2021-03-03 17:00:17 +0000

    Update watch.ino

--- watch.ino
+++ watch.ino
@@ -1,44 +1,40 @@
 /*
  Name:		watch.ino
  Created:	2021/2/27 22:30:44
+ Last modified:   2021/3/3
  Author:	yiyison, ps2g19
 */
 
 
 #include "watch.h"
-#include "TimerOne.h"
 
+Server_related server;
+Data_monitoring data_moni;
 
-#define heartrate_pin A0
-#define vibrate_pin A1
-#define beep_pin A2
-
-
-Display Display;
-Response_from_sever Response_from_sever;
-Data_monitoring Data_monitoring;
-
-
-char ssid[] = "qqq";    //network SSID (name)
-char password[] = "***";    //network password
-
+const char* ssid = "qqq";    //network SSID (name)
+const char* password = "***";    //network password
+const char* mqttServer = "3.133.147.7";
 
 // the setup function runs once when you press reset or power the board
 void setup() {
     /*
       I/O pin setups
     */
+
     Serial.begin(9600);
     Wire.begin();
-    dis.ini();    //initialise screen
-    con_to_wifi(ssid, password);    //connect to specified wifi
-    attachInterrupt(digitalPinToInterrupt(5), RECEIVE, mode);    //set RECEIVE as interrupt function
-    Timer1.initialize(30000000);    //initialise Timer1, interrupt every 30s
-    Timer1.attachInterrupt(SEND);    //set SEND as interrupt function
-    Data_monitoring.init_step_tracker();
+    data_moni.init_screen();
+    data_moni.init_step_tracker();
+
+    //attachInterrupt(digitalPinToInterrupt(5), receive, RISING);    //set RECEIVE as interrupt function
+
+    server.connectWifi(ssid, password);    //connect to specified wifi
+    server.connectMQTTServer(mqttServer);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  Data_monitoring.step_tracker();
+    server.loop();
+    send(data_moni);
+    data_moni.step_tracker();
 }
